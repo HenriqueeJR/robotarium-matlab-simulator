d4ad0e508045498c2174b836a9efbdc566a0b3a1
HenriqueeJR <91606600+HenriqueeJR@users.noreply.github.com>
2026-04-29 18:01:50 -0300

    Cleaning repository

--- examples/cbf_circulo/wrapper_cost.m
+++
@@ -1,3 +0,0 @@
-function cost = wrapper_cost(u, params)
-    cost = build_robot_cbf_circular(u, params);
-end
--- examples/cbf_circulo/wrapper_grad.m
+++
@@ -1,3 +0,0 @@
-function grad = wrapper_grad(u, params)
-    [~, grad] = build_robot_cbf_circular(u, params);
-end
